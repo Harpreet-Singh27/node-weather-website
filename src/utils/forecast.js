5b8322dc08ddc04bb06e05bc2640b689f7a0da1f
Harpreet Singh <60459961+Harpreet-Singh27@users.noreply.github.com>
2021-03-19 10:48:08 +0530

    Update forecast.js
    Removing my appId

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=58e6dd9959dbed9c390ef65353c7a903&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid={PutYourOwnAppId}&units=metric`;
   request({ url: url, json: true }, (error, response) => {
     if (error) {
       callback('Error occurred!!!!!');
